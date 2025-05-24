d9d06f036a99a5526768e2e478fdf850a368611c
SHEFIN FRANKLIN <shefinfranklin03@gmail.com>
2025-05-24 23:15:41 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,8 @@
-// frontend/tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Crucial: This tells Tailwind to scan all JS, TS, JSX, TSX files in src
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
