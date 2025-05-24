4af4e8b546b49a969a8d0edb4cfeddb4ca0c1b05
SHEFIN FRANKLIN <shefinfranklin03@gmail.com>
2025-05-24 23:07:42 +0530

    Update Chat.js

--- backend/models/Chat.js
+++ backend/models/Chat.js
@@ -2,27 +2,22 @@ const mongoose = require('mongoose');
 
 const chatSchema = mongoose.Schema(
     {
+        chatName: { type: String, trim: true },
+        isGroupChat: { type: Boolean, default: false },
         participants: [
             {
                 type: mongoose.Schema.Types.ObjectId,
                 ref: 'User',
             },
         ],
-        messages: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Message',
-            },
-        ],
         lastMessage: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Message',
         },
-        isGroupChat: { // To distinguish from group chats later
-            type: Boolean,
-            default: false,
+        groupAdmin: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
         },
-        // We might add a 'name' field for group chats later, but for 1-1 it's implied by participants
     },
     {
         timestamps: true,
