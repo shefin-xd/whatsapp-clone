d00e1182e5ecf017f4b1be0d323e2af6ea510781
SHEFIN FRANKLIN <shefinfranklin03@gmail.com>
2025-05-24 23:12:35 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,76 +1,9 @@
-// backend/routes/authRoutes.js (Confirm this is correct)
 const express = require('express');
-const asyncHandler = require('express-async-handler');
-const User = require('../models/User');
-const generateAccessToken = require('../utils/generateToken'); // Make sure this is correctly imported
+const { registerUser, loginUser } = require('../controllers/authController');
 
 const router = express.Router();
 
-// @desc    Register a new user
-// @route   POST /api/auth/register
-// @access  Public
-router.post(
-    '/register',
-    asyncHandler(async (req, res) => {
-        const { username, email, password } = req.body;
-
-        const userExists = await User.findOne({ email });
-
-        if (userExists) {
-            res.status(400);
-            throw new Error('User already exists with this email');
-        }
-
-        const usernameExists = await User.findOne({ username });
-        if (usernameExists) {
-            res.status(400);
-            throw new Error('Username already taken');
-        }
-
-        const user = await User.create({
-            username,
-            email,
-            password,
-        });
-
-        if (user) {
-            res.status(201).json({
-                _id: user._id,
-                username: user.username,
-                email: user.email,
-                profilePicture: user.profilePicture,
-                token: generateAccessToken(user._id),
-            });
-        } else {
-            res.status(400);
-            throw new Error('Invalid user data');
-        }
-    })
-);
-
-// @desc    Authenticate user & get token (Login)
-// @route   POST /api/auth/login
-// @access  Public
-router.post(
-    '/login',
-    asyncHandler(async (req, res) => {
-        const { email, password } = req.body;
-
-        const user = await User.findOne({ email });
-
-        if (user && (await user.matchPassword(password))) {
-            res.json({
-                _id: user._id,
-                username: user.username,
-                email: user.email,
-                profilePicture: user.profilePicture,
-                token: generateAccessToken(user._id),
-            });
-        } else {
-            res.status(401);
-            throw new Error('Invalid email or password');
-        }
-    })
-);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
