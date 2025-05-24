1fd4f3d3687a299b0020875c883fbab069bfbee7
SHEFIN FRANKLIN <shefinfranklin03@gmail.com>
2025-05-24 23:16:25 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,7 @@
-// frontend/src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <-- Make sure this line exists and points to your main CSS file
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
