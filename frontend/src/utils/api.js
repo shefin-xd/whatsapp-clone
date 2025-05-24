b8a0117fe260fe81922a247bb52c3abf5969e553
SHEFIN FRANKLIN <shefinfranklin03@gmail.com>
2025-05-24 22:39:12 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,23 @@
-// frontend/src/utils/api.js
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://shefin-whatsapp-clone.onrender.com/api', // Your backend API base URL
+    baseURL: 'https://shefin-whatsapp-clone.onrender.com/api',
 });
+
+// Request interceptor to add the authorization token
+api.interceptors.request.use(
+    (config) => {
+        const user = JSON.parse(localStorage.getItem('userInfo')); // Get user info from localStorage
+        const token = user ? user.token : null; // Extract the token
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`; // Attach the token
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
