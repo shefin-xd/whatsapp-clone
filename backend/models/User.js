6ddf66d0d7735ef2a053a0e49a3f9855f6bc20a5
SHEFIN FRANKLIN <shefinfranklin03@gmail.com>
2025-05-24 18:49:33 +0530

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        profilePicture: {
+            type: String,
+            default: 'https://icon-library.com/images/default-user-icon/default-user-icon-8.jpg', // Default avatar
+        },
+        status: {
+            type: String,
+            enum: ['online', 'offline', 'typing...'],
+            default: 'offline',
+        },
+        lastSeen: {
+            type: Date,
+        },
+    },
+    {
+        timestamps: true, // Adds createdAt and updatedAt fields
+    }
+);
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
