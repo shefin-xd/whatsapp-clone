375c2acd9eb8a5c417aa1fa9159fef6a556feead
SHEFIN FRANKLIN <shefinfranklin03@gmail.com>
2025-05-24 22:00:15 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
-// backend/controllers/userController.js
+// backend/controllers/userController.js (NEW)
 const asyncHandler = require('express-async-handler');
 const User = require('../models/User');
-const generateToken = require('../utils/generateToken');
+// const generateToken = require('../utils/generateToken'); // No longer needed here if only allUsers is exported
 
 // @desc    Get all users or search users
 // @route   GET /api/users?search=keyword
@@ -24,44 +24,4 @@ const allUsers = asyncHandler(async (req, res) => {
     res.send(users);
 });
 
-// @desc    Register a new user (already covered)
-// @route   POST /api/users/register
-// @access  Public
-const registerUser = asyncHandler(async (req, res) => {
-    // ... (your existing registerUser logic)
-    const { username, email, password } = req.body;
-
-    const userExists = await User.findOne({ email });
-
-    if (userExists) {
-        res.status(400);
-        throw new Error('User already exists with this email');
-    }
-
-    const usernameExists = await User.findOne({ username });
-    if (usernameExists) {
-        res.status(400);
-        throw new Error('Username already taken');
-    }
-
-    const user = await User.create({
-        username,
-        email,
-        password,
-    });
-
-    if (user) {
-        res.status(201).json({
-            _id: user._id,
-            username: user.username,
-            email: user.email,
-            profilePicture: user.profilePicture,
-            token: generateToken(user._id),
-        });
-    } else {
-        res.status(400);
-        throw new Error('Invalid user data');
-    }
-});
-
-module.exports = { allUsers, registerUser };
+module.exports = { allUsers }; // Only export allUsers
